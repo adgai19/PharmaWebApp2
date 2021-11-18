aa69a67b5cea13cdf3dd36c595798071a6308737
adgai19 <gaitondeaditya@gmail.com>
2021-11-18 19:40:53 +0530

    admin auth

--- src/main/java/com/psl/miniProject/controller/AdminProductsController.java
+++ src/main/java/com/psl/miniProject/controller/AdminProductsController.java
@@ -11,6 +11,8 @@ import java.util.Map;
 import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +26,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserRecord;
+import com.psl.miniProject.auth.SecurityService;
+import com.psl.miniProject.auth.models.User;
 import com.psl.miniProject.modal.Products;
 import com.psl.miniProject.repository.AdminProductsRepository;
 import com.psl.miniProject.modal.Ratings;
@@ -35,13 +41,11 @@ import com.psl.miniProject.repository.RatingsRepository;
 public class AdminProductsController {
 
 
+    @Autowired
+    SecurityService securityService;
 
     @Autowired
     private RatingsRepository ratingsRepository;
-
-
-
-
 
     public static String uploadDir = System.getProperty("user.dir")+"/src/main/resources/static/imgs";
 
@@ -50,12 +54,14 @@ public class AdminProductsController {
 
 
     @GetMapping("/products")
-    public List<Products> getAllProducts() {
-        return (List<Products>) adminProductsRepository.findAll();
+    public ResponseEntity getAllProducts() {
+        return idVerify(ResponseEntity.ok((List<Products>) adminProductsRepository.findAll()));
+
     }
 
     @PostMapping(value="/products",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public Products createProduct(@RequestPart("product") String product,@RequestPart("file") MultipartFile file) {
+    public ResponseEntity createProduct(@RequestPart("product") String product,
+                                        @RequestPart("file") MultipartFile file) {
         Products p1 = new Products();
         ObjectMapper objm = new ObjectMapper();
         try {
@@ -75,19 +81,20 @@ public class AdminProductsController {
         pro.setProduct_photo(filename);
         adminProductsRepository.save(pro);
         ratingsRepository.save(rat);
-        return pro;
+        return idVerify(ResponseEntity.ok(pro));
     }
 
     @GetMapping("/products/{id}")
-    public ResponseEntity<Products> getProductsById(@PathVariable("id") int id) throws Exception {
+    public ResponseEntity getProductsById(@PathVariable("id") int id) throws Exception {
         Products product = adminProductsRepository.
                            findById(id).orElseThrow(() -> new Exception("Product not found with ID: "+id));
-        return ResponseEntity.ok(product);
+        return idVerify(ResponseEntity.ok(product));
+
     }
 
     @PutMapping("/products/{id}")
-    public ResponseEntity<Products> updateProducts(@PathVariable("id") int id,
-            @RequestBody Products productDetails) throws Exception {
+    public ResponseEntity updateProducts(@PathVariable("id") int id,
+                                         @RequestBody Products productDetails) throws Exception {
         Products product = adminProductsRepository.
                            findById(id).orElseThrow(() -> new Exception("Product not found with ID: "+id));
         product.setCategory(productDetails.getCategory());
@@ -95,17 +102,30 @@ public class AdminProductsController {
         product.setPrice(productDetails.getPrice());
         product.setStock(productDetails.getStock());
         Products updatedProduct = adminProductsRepository.save(product);
-        return ResponseEntity.ok(updatedProduct);
+        return idVerify(ResponseEntity.ok(updatedProduct));
+
     }
 
     @DeleteMapping("/products/{id}")
-    public ResponseEntity<Map<String,Boolean>> deleteProducts(@PathVariable("id") int id) throws Exception {
+    public ResponseEntity deleteProducts(@PathVariable("id") int id) throws Exception {
         Products product = adminProductsRepository.
                            findById(id).orElseThrow(() -> new Exception("Product not found with ID: "+id));
         adminProductsRepository.delete(product);
         Map<String,Boolean> map = new HashMap<>();
         map.put("deleted",Boolean.TRUE);
-        return ResponseEntity.ok(map);
+        return idVerify(ResponseEntity.ok(map));
     }
 
+    // helper method for user auth.
+    // bit hacky but will go with this for now
+    private <T> ResponseEntity idVerify(ResponseEntity<T> identity) {
+        System.out.println("I am helper");
+        String email=securityService.getUser().getEmail();
+        if (email.equals("gaitondeaditya@gmail.com")) {
+            return identity;
+        } else {
+            return ResponseEntity.badRequest().body("You are not an admin so cannot access this ");
+        }
+
+    }
 }
